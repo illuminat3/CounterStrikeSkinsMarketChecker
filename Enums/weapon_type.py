b990f1bce34e20925bc303581cf04a273c25b590
Matthew <mrawson@redev.co.uk>
2024-08-17 18:59:59 +0100

    Added weapon type and wear enums

---
+++ Enums/weapon_type.py
@@ -0,0 +1,67 @@
+from enum import Enum
+
+class WeaponType(Enum):
+    # Knives
+    BUTTERFLY_KNIFE = "Butterfly Knife"
+    KARAMBIT = "Karambit"
+    BAYONET = "Bayonet"
+    M9_BAYONET = "M9 Bayonet"
+    FLIP_KNIFE = "Flip Knife"
+    GUT_KNIFE = "Gut Knife"
+    HUNTSMAN_KNIFE = "Huntsman Knife"
+    FALCHION_KNIFE = "Falchion Knife"
+    SHADOW_DAGGERS = "Shadow Daggers"
+    BOWIE_KNIFE = "Bowie Knife"
+    CLASSIC_KNIFE = "Classic Knife"
+    NOMAD_KNIFE = "Nomad Knife"
+    PARACORD_KNIFE = "Paracord Knife"
+    SURVIVAL_KNIFE = "Survival Knife"
+    URSUS_KNIFE = "Ursus Knife"
+    TALON_KNIFE = "Talon Knife"
+    KUKRI_KNIFE = "Kukri Knife"
+    
+    # Pistols
+    GLOCK_18 = "Glock-18"
+    USP_S = "USP-S"
+    P2000 = "P2000"
+    DUAL_BERETTAS = "Dual Berettas"
+    P250 = "P250"
+    FIVE_SEVEN = "Five-SeveN"
+    CZ75_AUTO = "CZ75-Auto"
+    TEC_9 = "Tec-9"
+    DESERT_EAGLE = "Desert Eagle"
+    R8_REVOLVER = "R8 Revolver"
+    
+    # SMGs
+    MAC_10 = "MAC-10"
+    MP9 = "MP9"
+    MP7 = "MP7"
+    MP5_SD = "MP5-SD"
+    UMP_45 = "UMP-45"
+    P90 = "P90"
+    PP_BIZON = "PP-Bizon"
+    
+    # Rifles
+    AK_47 = "AK-47"
+    M4A4 = "M4A4"
+    M4A1_S = "M4A1-S"
+    AUG = "AUG"
+    SG_553 = "SG 553"
+    FAMAS = "FAMAS"
+    GALIL_AR = "Galil AR"
+    
+    # Sniper Rifles
+    AWP = "AWP"
+    SSG_08 = "SSG 08"
+    SCAR_20 = "SCAR-20"
+    G3SG1 = "G3SG1"
+    
+    # Shotguns
+    NOVA = "Nova"
+    XM1014 = "XM1014"
+    MAG_7 = "MAG-7"
+    SAWED_OFF = "Sawed-Off"
+    
+    # Machine Guns
+    M249 = "M249"
+    NEGEV = "Negev"
---
+++ Enums/weapon_wear.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class WeaponWear(Enum):
+    FACTORY_NEW = "Factory New"
+    MINIMAL_WEAR = "Minimal Wear"
+    FIELD_TESTED = "Field-Tested"
+    WELL_WORN = "Well-Worn"
+    BATTLE_SCARRED = "Battle-Scarred"
